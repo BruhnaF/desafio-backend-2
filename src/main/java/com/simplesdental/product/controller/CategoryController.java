ce0080d2e3f2ca80aed95bc16df6ab61e1f256b3
BruhnaF <bruhnaformento@gmail.com>
2025-04-21 21:59:25 -0300

    refactor: refatorado controller para implementar request/response apenas com as propriedades esperadas

--- src/main/java/com/simplesdental/product/controller/CategoryController.java
+++ src/main/java/com/simplesdental/product/controller/CategoryController.java
@@ -1,25 +1,34 @@
 package com.simplesdental.product.controller;
 
-import com.simplesdental.product.model.Category;
-import com.simplesdental.product.service.CategoryService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simplesdental.product.model.Category;
+import com.simplesdental.product.service.Result;
+import com.simplesdental.product.service.category.CategoryService;
+import com.simplesdental.product.service.category.CreateRequest;
+import com.simplesdental.product.service.category.CreateResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
-
-    @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping
     public List<Category> getAllCategories() {
@@ -35,18 +44,23 @@ public class CategoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Category createCategory(@Valid @RequestBody Category category) {
-        return categoryService.save(category);
+    public ResponseEntity<Object> createCategory(@RequestBody CreateRequest category) {
+        Result<CreateResponse> response = categoryService.save(category);
+        if (!response.isSuccess()) {
+            return ResponseEntity.badRequest().body(response.getErrors());
+        }
+        return ResponseEntity.ok(response.getData());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @Valid @RequestBody Category category) {
-        return categoryService.findById(id)
+    public ResponseEntity<Object> updateCategory(@PathVariable Long id, @Valid @RequestBody Category category) {
+        /*return categoryService.findById(id)
                 .map(existingCategory -> {
                     category.setId(id);
                     return ResponseEntity.ok(categoryService.save(category));
                 })
-                .orElse(ResponseEntity.notFound().build());
+                .orElse(ResponseEntity.notFound().build());*/
+                return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
